43a58a665e539e9c454dbb7aefd2b6ef35e25639
AR test <sekai.jinmu.tenno@gmail.com>
2023-12-04 12:19:33 +0600

    updated

--- git_command.py
+++ git_command.py
@@ -6,3 +6,13 @@
 # аналогии Gitlab, Bitbucket
 # ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRVpQdcWQOHDUY3fL11H9g2nCG5YRm9bvLkGmWwAmCCSFJ8qF8xUaMnIMDsDga33yL1NVli4jz4lNrsIyxR7Rk1w4cO4dpJuCDavXFqSgwo2nvisrV7E2tazYGj2Y6GUzbS6aXdjtsGj5sbtHrHxQpoO5G8MgraENndmI+Gkxx/TBtnalWurTkn7BFQRkWau05RDv9ehS6lbAjqrETQpNO3NeAhdd114al59rklzd1VPdgfirOMxB1E/ssnx0rPyQLNYouKeHoH2JczkK625LBZJfM5z5s8zWaTm4IM2hVkwNqzKXV8v+sMSH6D5LjqmeSN39HAFye6DxiK49SMU2vWvcuPpbPrt4m3NMskrY2U34VSAfKzXU8tJHmSnigevraBgU4FgBXhdLqFzY52jBwLKkkw1ogObjNqF00Srd1MWQroxWAflLoQ+neB+zmniyMp3/v0z0OHiqD/4w+kizqxPt7uipxqLRVIY0B6B6kgACDgpr3Gf/O2KLTNXkcwZU= amangeldyaidekov@MacBook-Air-Amangeldy.local
 
+'''команды'''
+# git init -> превращает дирекорию в git репозиторий (инициализирует директорию и появляется скрытая директория .git) вводиться только один  раз 
+
+# gir add . (все содержимое директории) / <filaname> (конкретный файл git add test.py) -> отслеживание файлов или файла gitом 
+
+# git commit -m 'message' -> создание коммита с комментарием, с сохраниением в git (локальном на компе) 
+
+# git remote add <название_связи>(обычно origin) ссылка_на_удаленный_репозиторий (HTTPS/SSH) -> связывает репозиторий с удаленным 
+
+# git push <название _связи> <назвагние-ветки>(master / main) -> отправке версий кода на удаленный репозиторий (то есть на git hub)
