c8d06eb5e85aa4cf240ac97fc09ee9eedc9f235c
AR test <sekai.jinmu.tenno@gmail.com>
2023-12-04 11:56:29 +0600

    first commit

---
+++ git_command.py
@@ -0,0 +1,8 @@
+'''GIZ'''
+# распределенная система контроля версий (система, записывать изменения в файле или наборе файлов). Использует для отслеживания и введенения истории изменений файлов, позволяет вернуть старую версию кода.
+# '''GitHub'''
+
+#ондайн сервис - который представляет услуги по хранению репозиториев и управления доступом
+# аналогии Gitlab, Bitbucket
+# ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRVpQdcWQOHDUY3fL11H9g2nCG5YRm9bvLkGmWwAmCCSFJ8qF8xUaMnIMDsDga33yL1NVli4jz4lNrsIyxR7Rk1w4cO4dpJuCDavXFqSgwo2nvisrV7E2tazYGj2Y6GUzbS6aXdjtsGj5sbtHrHxQpoO5G8MgraENndmI+Gkxx/TBtnalWurTkn7BFQRkWau05RDv9ehS6lbAjqrETQpNO3NeAhdd114al59rklzd1VPdgfirOMxB1E/ssnx0rPyQLNYouKeHoH2JczkK625LBZJfM5z5s8zWaTm4IM2hVkwNqzKXV8v+sMSH6D5LjqmeSN39HAFye6DxiK49SMU2vWvcuPpbPrt4m3NMskrY2U34VSAfKzXU8tJHmSnigevraBgU4FgBXhdLqFzY52jBwLKkkw1ogObjNqF00Srd1MWQroxWAflLoQ+neB+zmniyMp3/v0z0OHiqD/4w+kizqxPt7uipxqLRVIY0B6B6kgACDgpr3Gf/O2KLTNXkcwZU= amangeldyaidekov@MacBook-Air-Amangeldy.local
+
